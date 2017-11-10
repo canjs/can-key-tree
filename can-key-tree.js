28e9ea8c5b0ddc4c65040f01e46c8856b7c1844f
James Atherton <james@bitovi.com>
2017-11-10 15:31:58 -0500

    Adding changes Justin requested.

--- can-key-tree.js
+++ can-key-tree.js
@@ -143,8 +143,8 @@ function getDeep ( item, items, depth, maxDepth ) {
  * keyTree.get(["click"]) //-> [handler1, handler2, handler3]
  * ```
  *
- * @param {Array} [keys] An array of keys to specify where to start recursively getting leafs from
- * @return {KeyTree} The keyTree
+ * @param {Array} [keys] An array of keys to specify where to start recursively getting leafs from.
+ * @return {Array} The leafs from keyTree under the specified `keys` path.
  */
 KeyTree.prototype.get = function ( keys ) {
 	var place = this.getNode( keys );
@@ -160,6 +160,37 @@ KeyTree.prototype.get = function ( keys ) {
 	}
 };
 
+/**
+ * @function can-key-tree.prototype.getNode getNode
+ * @parent can-key-tree.prototype
+ *
+ * @signature `keyTree.getNode(keys)`
+ *
+ * Return the node from the keyTree specified by `keys`.
+ *
+ * Given this setup:
+ *
+ * ```js
+ * var keyTree = new KeyTree( [Object, Object, Array] );
+ *
+ * function handler1(){}
+ * function handler2(){}
+ * function handler3(){}
+ *
+ * keyTree.add(["click", "li", handler1]);
+ * keyTree.add(["click", "li", handler2]);
+ * keyTree.add(["click", "span", handler3]);
+ * ```
+ *
+ * To get all the object at `click`, use `.getNode`:
+ *
+ * ```js
+ * keyTree.getNode(["click"]) //-> { li: [handler1, handler2], span: [handler2] }
+ * ```
+ *
+ * @param {Array} [keys] An array of keys to specify where to pull the node from.
+ * @return {Object|Array|Value} The node from the keyTree specified by the 'keys' path.
+ */
 KeyTree.prototype.getNode = function ( keys ) {
 	var place = this.root;
 	for ( var i = 0; i < keys.length; i++ ) {
--- docs/types.keyTreeCallbacks.md
+++ docs/types.keyTreeCallbacks.md
@@ -1,6 +1,8 @@
 @typedef {Object} can-key-tree.types.keyTreeCallbacks KeyTreeCallbacks
 @parent can-key-tree.types
 
+@description Defines callbacks `onFirst` and/or `onEmpty`.
+
 @type {Object} Defines callbacks `onFirst` and/or `onEmpty`.
 
 ```js
@@ -22,3 +24,8 @@ keyTree.add( ["click", "li", handler1] );
 keyTree.delete( [] );
 // onEmpty is called with a context (`this`) of the keyTree instance and no arguments
 ```
+
+  @option {Function} [onFirst] called with a context (`this`) of the keyTree instance and no arguments when the tree gets its first item added.
+
+  @option {Function} [onEmpty] called with a context (`this`) of the keyTree instance and no arguments when the tree becomes empty.
+
